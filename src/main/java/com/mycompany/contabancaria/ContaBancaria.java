27bf7d9ac8a813f48a05992acbafa1ba122e1488
Diego Nunes <nunesdiegond@gmail.com>
2024-04-30 15:06:28 -0300

    exercicio concluido

--- src/main/java/com/mycompany/contabancaria/ContaBancaria.java
+++ src/main/java/com/mycompany/contabancaria/ContaBancaria.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.contabancaria;
 
-/**
- *
- * @author nunes
- */
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaBancaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("#,##0.00");
+
+        System.out.println("Bem vindo ao Banco Nunes!");
+
+        System.out.print("Por favor, informe seu Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print(nome + ", por favor insira o número de sua agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.print("Agora o número de sua conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Quanto irá depositar na conta: " + conta + " da agência "+agencia + "?");
+        double saldo = scanner.nextDouble();
+        String saldoFormatado = formatador.format(saldo);
+
+        System.out.println("Cliente " + nome + "o saldo da conta " + conta + ", na agencia "+ agencia + ", é de R$"+saldoFormatado + "e já se encotra disponível para saque");
+
     }
 }
